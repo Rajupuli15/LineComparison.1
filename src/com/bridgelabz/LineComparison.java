2ee1f4bf7920052b1a4cc26a753610e00b78ea62
Rajupuli15 <Rajupuli15@gmail.com>
2021-09-08 02:49:25 -0400

    Added:UC1 line lenght

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,8 +1,24 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
 public class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line comparison");
-    }
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the coordinates (x1.y1)");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+
+        System.out.println("Enter the coordinates (x2,y2)");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        int res1 = (int) Math.pow((x2-x1), 2);
+        int res2 = (int) Math.pow((y2-y1), 2);
+
+        double length = Math.sqrt(res1+res2);
+
+        System.out.println("Length of line:"+ length);
+    }
 }
+
